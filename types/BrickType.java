5e582571a5614e816ba89e4c0fd64d7e933c4fe3
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-04 21:29:02 +0700

    chore: initial commit with BrickType.java

--- types/BrickType.java
+++ types/BrickType.java
@@ -1,4 +1,21 @@
 package types;
 
+/** Represents the type of brick */
 public class BrickType {
+    private final String name;
+    private final int durability;
+
+    /** Constructor */
+    public BrickType(String name, int durability) {
+        this.name = name;
+        this.durability = durability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
 }
