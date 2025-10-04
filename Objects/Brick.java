ebceb12c8c946d610f43a7f3c5d7e6324dcb519b
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-04 21:25:06 +0700

    chore: initial commit with Brick.java

--- Objects/Brick.java
+++ Objects/Brick.java
@@ -1,4 +1,59 @@
 package Objects;
+import java.awt.*;
+import types.BrickType;
 
-public class Brick {
+/**
+ * Represents a single brick in the game.
+ */
+public class Brick extends GameObjects {
+    private int hitPoints;
+    private final BrickType type;
+    private Color color;
+    private boolean destroyed = false;
+
+    /** Constructor */
+    public Brick(float x, float y, int width, int height, int hitPoints, BrickType type) {
+        super(x, y, width, height);
+        this.hitPoints = hitPoints;
+        this.type = type;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setColor(Color newColor) {
+        this.color = newColor;
+    }
+
+    /**
+     * Reduces hit points.
+     * Destroys brick if zero.
+     */
+    public void takeHit() {
+        if (!destroyed) {
+            hitPoints--;
+            if (hitPoints <= 0) {
+                destroyed = true;
+            }
+        }
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    /** Update logic */
+    @Override
+    public void update() {
+    }
+
+    /** Draws the brick */
+    @Override
+    public void render(Graphics g) {
+        if (!destroyed) {
+            g.setColor(color);
+            g.fillRect((int) x, (int) y, width, height);
+        }
+    }
 }
