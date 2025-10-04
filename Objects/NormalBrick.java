1f43f23310f16c58d7b2ac2dcb09f30133e0481f
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-04 21:26:11 +0700

    chore: initial commit with NormalBrick.java

--- Objects/NormalBrick.java
+++ Objects/NormalBrick.java
@@ -1,4 +1,23 @@
 package Objects;
+import types.BrickType;
+import java.awt.Color;
 
-public class NormalBrick {
+/**
+ * Represents a normal brick with 1 hit point.
+ */
+public class NormalBrick extends Brick {
+    /** Constructor */
+    public NormalBrick(float x, float y, int width, int height) {
+        super(x, y, width, height, 1, new BrickType("Normal", 1));
+        setColor(Color.CYAN);
+    }
+
+    /**
+     * Handles a hit.
+     * Normal brick is destroyed after 1 hit.
+     */
+    @Override
+    public void takeHit() {
+        super.takeHit();
+    }
 }
