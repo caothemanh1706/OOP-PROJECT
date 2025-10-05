c07b653866c3707c92edd078f819decc943ba021
hng-q <qh75884@gmail.com>
2025-10-05 09:56:29 +0700

    feat(game): add core game object classes
    Ball, Paddle, MovableObject, GameObjects

--- Objects/GameObjects.java
+++ Objects/GameObjects.java
@@ -1,4 +1,19 @@
 package Objects;
 
-public class GameObjects {
+import java.awt.*;
+
+public abstract class GameObjects {
+    protected float x;
+    protected float y;
+    protected int width;
+    protected int height;
+
+    public GameObjects(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public abstract void update();
+    public abstract void render(Graphics g);
 }
