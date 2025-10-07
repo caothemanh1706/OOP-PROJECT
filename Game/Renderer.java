74a9e9c31fd28ae6a082c9b8d5d88754f1557b9f
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-10-07 09:49:00 +0700

    feat(renderer): add Renderer class for drawing game objects

--- Game/Renderer.java
+++ Game/Renderer.java
@@ -1,4 +1,48 @@
 package Game;
 
-public class Renderer {
+import Objects.Ball;
+import Objects.GameObjects;
+import Objects.Paddle;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Renderer extends JPanel {
+    private final List<GameObjects> gameObjects = new ArrayList<>();
+
+    /**
+     * constructor 1 .
+     * setBackground color back.
+     */
+    public Renderer() {
+        setBackground(Color.BLACK);
+
+        Paddle paddle = new Paddle(350, 500, 100, 20, 0, 0, 5);
+        gameObjects.add(paddle);
+
+        Ball ball = new Ball (380, 460, 20, 20, 0,0 ,0f, 0f, 0f);
+        gameObjects.add(ball);
+    }
+
+    /**
+     * Add a new game object to the list.
+     */
+    public void addGameObject(GameObjects obj) {
+        gameObjects.add(obj);
+
+    }
+
+    /**
+     * Draw object.
+     */
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (GameObjects obj : gameObjects) {
+            obj.render(g);
+        }
+    }
+
 }
