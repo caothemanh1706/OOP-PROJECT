ef73b7434b7faa823c6e2c52248f695f3fb0681e
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-10 22:25:04 +0700

    feat(render): switch to using images instead of drawing

--- Objects/Brick.java
+++ Objects/Brick.java
@@ -1,6 +1,12 @@
 package Objects;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 import types.BrickType;
+
+import javax.imageio.ImageIO;
 
 /**
  * Represents a single brick in the game.
@@ -8,22 +14,49 @@ import types.BrickType;
 public class Brick extends GameObjects {
     private int hitPoints;
     private final BrickType type;
-    private Color color;
     private boolean destroyed = false;
+    private BufferedImage brickImage;
 
     /** Constructor */
-    public Brick(float x, float y, int width, int height, int hitPoints, BrickType type) {
+    public Brick(float x, float y, int width, int height, int hitPoints, BrickType type, String imagePath) {
         super(x, y, width, height);
         this.hitPoints = hitPoints;
         this.type = type;
+        loadImage(imagePath);
+    }
+
+    /** Loads brick image from path. */
+    private void loadImage(String path) {
+        try {
+            java.net.URL imgURL = getClass().getResource(path);
+            if (imgURL != null) {
+                brickImage = ImageIO.read(imgURL);
+            } else {
+                System.out.println("Brick image not found: " + path);
+                brickImage = null;
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot load brick image: " + path);
+            brickImage = null;
+        }
+    }
+
+    /** Changes brick image. */
+    public void changeImage(String newPath) {
+        try {
+            java.net.URL imgURL = getClass().getResource(newPath);
+            if (imgURL != null) {
+                brickImage = ImageIO.read(imgURL);
+            } else {
+                System.out.println("Brick image not found: " + newPath);
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot change the brick image: " + newPath);
+        }
     }
 
     public int getHitPoints() {
         return hitPoints;
-    }
-
-    public void setColor(Color newColor) {
-        this.color = newColor;
     }
 
     /**
@@ -51,9 +84,12 @@ public class Brick extends GameObjects {
     /** Draws the brick */
     @Override
     public void render(Graphics g) {
-        if (!destroyed) {
-            g.setColor(color);
-            g.fillRect((int) x, (int) y, width, height);
+        if (destroyed) {
+            return;
+        }
+
+        if (brickImage != null) {
+            g.drawImage(brickImage, (int) x, (int) y, width, height, null);
         }
     }
 }
