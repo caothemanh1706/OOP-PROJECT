21b22c2197da392c216ae2f2e76864a34ecb2c55
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-10 22:26:03 +0700

    feat(render): switch to using images instead of drawing

--- Objects/NormalBrick.java
+++ Objects/NormalBrick.java
@@ -1,6 +1,5 @@
 package Objects;
 import types.BrickType;
-import java.awt.Color;
 
 /**
  * Represents a normal brick with 1 hit point.
@@ -8,12 +7,11 @@ import java.awt.Color;
 public class NormalBrick extends Brick {
     /** Constructor */
     public NormalBrick(float x, float y, int width, int height) {
-        super(x, y, width, height, 1, new BrickType("Normal", 1));
-        setColor(Color.CYAN);
+        super(x, y, width, height, 1,
+                new BrickType("Normal", 1), "/assets/block01.png");
     }
 
     /**
-     * Handles a hit.
      * Normal brick is destroyed after 1 hit.
      */
     @Override
