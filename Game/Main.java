d56dddf3c08a731446536a606483d6bc32ca3e63
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-10-14 08:46:47 +0700

    feat(main): add game loop and initialize GameManager

--- Game/Main.java
+++ Game/Main.java
@@ -1,9 +1,6 @@
 package Game;
 
-import Objects.Brick;
-import Objects.NormalBrick;
-import Objects.Paddle;
-import Objects.StrongBrick;
+import Objects.*;
 import types.BrickType;
 
 import javax.swing.*;
@@ -13,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Arkanoid");
         Renderer renderer = new Renderer();
+        int ScreenHeight = 600;
+        int ScreenWidth = 800;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         int startX = 50;
         int y = 100;
@@ -20,19 +19,33 @@ public class Main {
         int brickHeight = 20;
         int spacing = 10;
 
+        GameManager manager = GameManager.getInstance();
         for (int i = 0; i < 10; i++) {
             int x = startX + i * (brickWidth + spacing);
+            Brick brick;
             if (i % 2 == 0) {
-                renderer.addGameObject(new NormalBrick(x, y, brickWidth, brickHeight));
+                brick = new NormalBrick(x, y, brickWidth, brickHeight);
             } else {
-                renderer.addGameObject(new StrongBrick(x, y, brickWidth, brickHeight));
+                brick = new StrongBrick(x, y, brickWidth, brickHeight);
             }
+            renderer.addGameObject(brick);
+            manager.getBricks().add(brick);
         }
-
-        frame.setSize(800,600);
+        frame.setSize(ScreenWidth,ScreenHeight);
         frame.setResizable(false);
         frame.add(renderer);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+
+        // Game loop ~60 FPS
+        while (true) {
+            manager.updateGame();
+            try {
+                Thread.sleep(16); // 1000ms / 60fps ≈ 16ms
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
