2ad1452d9eaff2334ea35cd1d10e52206bf22a0e
tuanthao2411 <nguyentuanthaodeeptry@gmail.com>
2025-10-14 17:16:40 +0700

    Add setBackground and addgameObject in Renderer

--- Game/Renderer.java
+++ Game/Renderer.java
@@ -1,48 +1,64 @@
 package Game;
 
-import Objects.Ball;
-import Objects.GameObjects;
-import Objects.Paddle;
-
+import Objects.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Renderer extends JPanel {
     private final List<GameObjects> gameObjects = new ArrayList<>();
+    private BufferedImage background;
+    private Paddle paddle;
+    private Ball ball;
 
-    /**
-     * constructor 1 .
-     * setBackground color back.
-     */
     public Renderer() {
-        setBackground(Color.BLACK);
+        try {
+            background = ImageIO.read(getClass().getResourceAsStream("/assets/background1.png"));
+        } catch (IOException | IllegalArgumentException e) {
+            System.err.println("Background not found.");
+            background = null;
+        }
 
-        Paddle paddle = new Paddle(350, 500, 100, 20, 0, 0, 5);
+        setBackground(Color.BLACK);
+        paddle = new Paddle(350, 500, 100, 20, 0, 0, 5);
         gameObjects.add(paddle);
 
-        Ball ball = new Ball (380, 460, 20, 20, 0,0 ,0f, 0f, 0f);
+        ball = new Ball(paddle.getX() + paddle.getWidth() / 2 - 10,
+                paddle.getY() - 20 - 5,
+                20, 20, 0, 0, 0f, 0f, 5f,
+                800, 600, "/assets/ball.png"
+         );
+
         gameObjects.add(ball);
+        GameManager.getInstance().startGame(this, paddle, ball);
     }
 
-    /**
-     * Add a new game object to the list.
-     */
     public void addGameObject(GameObjects obj) {
         gameObjects.add(obj);
-
     }
 
-    /**
-     * Draw object.
-     */
+    public Paddle getPaddle() {
+        return paddle;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        if (background != null) {
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        }
 
         for (GameObjects obj : gameObjects) {
             obj.render(g);
         }
     }
-
 }
