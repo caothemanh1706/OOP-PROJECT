d26f60d4a9e15a27987569f3efff91381b9ff4d0
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-10-14 08:50:42 +0700

    feat: initial project setup

--- Game/GameManager.java
+++ Game/GameManager.java
@@ -1,4 +1,44 @@
 package Game;
 
+import Objects.Ball;
+import Objects.Brick;
+import Objects.Paddle;
+import powerups.PowerUp;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Quản lý toàn bộ logic của game Arkanoid.
+ */
 public class GameManager {
+    private static GameManager instance;
+    private Paddle paddle;
+    private Ball ball;
+    private List<Brick> bricks;
+    private List<PowerUp> powerUps;
+    private int score = 0;
+    private int lives = 3;
+    private String gameState;
+    private Renderer renderer;
+
+    /**
+     * constructor gameManager.
+     */
+    public GameManager() {
+        this.bricks = new ArrayList<>();
+        this.powerUps = new ArrayList<>();
+        this.score = 0;
+        this.lives = 3;
+        this.gameState = "READY";
+    }
+
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
 }
