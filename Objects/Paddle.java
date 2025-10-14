61ee27f20dac4f25576d028b912524d6ba118149
hng-q <qh75884@gmail.com>
2025-10-14 21:48:49 +0700

    feat(paddle): add stop method, boundary collision.

--- Objects/Paddle.java
+++ Objects/Paddle.java
@@ -3,6 +3,7 @@ package Objects;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
 
 public class Paddle extends MovableObject {
     private float speed;
@@ -19,6 +20,9 @@ public class Paddle extends MovableObject {
     public void moveRight() {
         dx = speed;
     }
+    public void stop() {
+        dx = 0;
+    }
 
     @Override
     public void render(Graphics g) {
@@ -30,6 +34,25 @@ public class Paddle extends MovableObject {
     @Override
     public void update() {
         x += dx;
+        if (x < 0) x = 0;
+        if (x + width > 785) x = 785 - width;
         y += dy;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, width, height);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() { return y; }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
     }
 }
