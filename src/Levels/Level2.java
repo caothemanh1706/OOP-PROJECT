fdd12b900c3c94d857faaa62d5de29683a649053
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-31 00:33:58 +0700

    refactor: change Level2 loading to use text file instead of hardcoded layout

--- src/Levels/Level2.java
+++ src/Levels/Level2.java
@@ -1,11 +1,13 @@
 package Levels;
 
 import Game.Renderer;
-import Objects.Brick;
-import Objects.StrongBrick;
-import Objects.NormalBrick;
+import Objects.*;
+
 import java.util.List;
 
+/**
+ * Defines layout and settings for Level 2.
+ */
 public class Level2 extends Level {
     public Level2() {
         super(100, 80, 60, 25, 0);
@@ -16,34 +18,9 @@ public class Level2 extends Level {
         return false;
     }
 
+    /** Loads level 2 from file. */
     @Override
     public void loadLevel(Renderer renderer, List<Brick> bricks) {
-        bricks.clear();
-
-        int totalRows = 8;
-
-        for (int row = 0; row < totalRows; row++) {
-            int bricksInRow = row + 1;
-            int totalWidth = bricksInRow * brickWidth;
-            int startX = (screenWidth - totalWidth) / 2;
-
-            for (int col = 0; col < bricksInRow; col++) {
-                int x = startX + col * (brickWidth + spacing);
-                int y = startY + row * (brickHeight + spacing);
-
-                Brick brick;
-
-                boolean isEdge = (row == 0 || row == totalRows - 1 || col == 0 || col == bricksInRow - 1);
-
-                if (isEdge) {
-                    brick = new StrongBrick(x, y, brickWidth, brickHeight);
-                } else {
-                    brick = new NormalBrick(x, y, brickWidth, brickHeight);
-                }
-
-                bricks.add(brick);
-                renderer.addGameObject(brick);
-            }
-        }
+        LevelLoader.loadFromFile("level2.txt", renderer, bricks, startX, startY, brickWidth, brickHeight, spacing);
     }
 }
