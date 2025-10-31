011d693feffca4269828cdab2df424d30bf12c8d
PXHieu <phamxuanhieu2k6@gmail.com>
2025-10-31 13:34:29 +0700

    refactor: change Level1 loading to use text file instead of hardcoded layout

--- src/Levels/Level1.java
+++ src/Levels/Level1.java
@@ -4,32 +4,22 @@ import Game.Renderer;
 import Objects.*;
 import java.util.List;
 
+/**
+ * Defines layout and settings for Level 1.
+ */
 public class Level1 extends Level {
     public Level1() {
-        super(100, 80, 60, 25, 0);
+        super(0, 60, 60, 25, 0);
     }
 
     @Override
     public boolean hasPowerUp() {
-        return false;
+        return true;
     }
 
+    /** Loads level 1 from file. */
     @Override
     public void loadLevel(Renderer renderer, List<Brick> bricks) {
-        int rows = 4;
-        int cols = 10;
-
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                int x = startX + col * (brickWidth + spacing);
-                int y = startY + row * (brickHeight + spacing);
-
-                Brick brick;
-                brick = new NormalBrick(x, y, brickWidth, brickHeight);
-
-                renderer.addGameObject(brick);
-                bricks.add(brick);
-            }
-        }
+        LevelLoader.loadFromFile("level1.txt", renderer, bricks, startX, startY, brickWidth, brickHeight, spacing);
     }
 }
