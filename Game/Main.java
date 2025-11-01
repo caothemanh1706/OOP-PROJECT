cc0250a09e1814f50bee51a9735e298f96184a00
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-11-02 00:00:02 +0700

    refactor: clean up Main.java initialization code

--- Game/Main.java
+++ Game/Main.java
@@ -1,51 +1,25 @@
 package Game;
 
 import Objects.*;
-import types.BrickType;
-
 import javax.swing.*;
 import java.awt.*;
-
 public class Main {
+    public static final int ScreenWidth = 800;
+    public static final int ScreenHeight = 600;
     public static void main(String[] args) {
         JFrame frame = new JFrame("Arkanoid");
         Renderer renderer = new Renderer();
-        int ScreenHeight = 600;
-        int ScreenWidth = 800;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        int startX = 50;
-        int y = 100;
-        int brickWidth = 60;
-        int brickHeight = 20;
-        int spacing = 10;
-
         GameManager manager = GameManager.getInstance();
-        for (int i = 0; i < 10; i++) {
-            int x = startX + i * (brickWidth + spacing);
-            Brick brick;
-            if (i % 2 == 0) {
-                brick = new NormalBrick(x, y, brickWidth, brickHeight);
-            } else {
-                brick = new StrongBrick(x, y, brickWidth, brickHeight);
-            }
-            renderer.addGameObject(brick);
-            manager.getBricks().add(brick);
-        }
         frame.setSize(ScreenWidth,ScreenHeight);
         frame.setResizable(false);
         frame.add(renderer);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-
-        // Game loop ~60 FPS
-        while (true) {
+        Timer timer = new Timer(16, e -> {
             manager.updateGame();
-            try {
-                Thread.sleep(16); // 1000ms / 60fps ≈ 16ms
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+            renderer.repaint();
+        });
+        timer.start();
     }
 }
